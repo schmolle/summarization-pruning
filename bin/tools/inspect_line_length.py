b6c153013d79d9ec109a8b5d27ac4024b9c2b068
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-04-22 11:01:20 +0200

    added prints

--- bin/tools/inspect_line_length.py
+++ bin/tools/inspect_line_length.py
@@ -1,6 +1,6 @@
 import gzip
 
-counter = 0
+counter = 3213835
 max_length = 0
 min_length = 0
 longer1024 = 0
@@ -10,5 +10,9 @@ with gzip.open('/data/ms-marco/fulldocs.tsv.gz','rt') as f:
 		length = len(line)
 		if length < min_length: min_length = length
 		if length > max_length: max_length = length
-		if length > 1024: longer1024 = counter + 1
-		counter = counter + 1
+		if length > 1024: longer1024 = longer1024 + 1
+
+print("TOTAL LINES %s" % (counter,))
+print("longer1024 %s" % (longer1024,))
+print("min_length %s" % (min_length,))
+print("max_length %s" % (max_length,))
