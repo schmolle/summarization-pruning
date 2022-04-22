1db84f500cad7be2a6dce056bd49c698c245d5c4
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-04-22 10:30:01 +0200

    added line max

--- bin/summarize.py
+++ bin/summarize.py
@@ -3,7 +3,10 @@ from transformers import pipeline
 
 with gzip.open('/data/ms-marco/fulldocs.tsv.gz','rt') as f:
 	l1 = f.readline()
+	l1 = l1[0:1024]
+	
 	l2 = f.readline()
+	l2 = l2[0:1024]
 
 	summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
 	
