4a92ec4bb077ea83ee646483250d06c79964a284
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-04-22 10:24:08 +0200

    added summarization

--- bin/summarize.py
+++ bin/summarize.py
@@ -1,4 +1,15 @@
 import gzip
+from transformers import pipeline
 
 with gzip.open('/data/ms-marco/fulldocs.tsv.gz','rt') as f:
-    print(f.readline())
+	l1 = f.readline()
+	l2 = f.readline()
+
+	summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+	
+	print(l1)
+	print(summarizer(l1, max_length=130, min_length=30, do_sample=False))
+	print("---------------------------------------------------------")
+	print(l2)
+
+	print(summarizer(l2, max_length=130, min_length=30, do_sample=False))
