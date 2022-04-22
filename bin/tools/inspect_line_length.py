055bd4ac3ca8c38552b1e739d27841271ed404f1
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-04-22 10:40:16 +0200

    bugfix

--- bin/tools/inspect_line_length.py
+++ bin/tools/inspect_line_length.py
@@ -1,9 +1,9 @@
 import gzip
 
-counter = 0;
-max_length = 0;
-min_length = 0;
-longer1024 = 0;
+counter = 0
+max_length = 0
+min_length = 0
+longer1024 = 0
 
 with gzip.open('/data/ms-marco/fulldocs.tsv.gz','rt') as f:
 	for line in f:
@@ -11,4 +11,4 @@ with gzip.open('/data/ms-marco/fulldocs.tsv.gz','rt') as f:
 		if length < min_length: min_length = length
 		if length > max_length: max_length = length
 		if length > 1024: longer1024 = counter + 1
-        counter = counter + 1
+		counter = counter + 1
