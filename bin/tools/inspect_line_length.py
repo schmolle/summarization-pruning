8324699d649bac90548f981cd7e84a7975570f1b
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-04-22 12:14:35 +0200

    added logging and exeception

--- bin/tools/inspect_line_length.py
+++ bin/tools/inspect_line_length.py
@@ -1,18 +1,23 @@
 import gzip
+import logging
 
-counter = 3213835
-max_length = 0
-min_length = 0
-longer1024 = 0
+logging.basicConfig(filename='inspect.log', encoding='utf-8', level=logging.DEBUG)
+try:
+	counter = 3213835
+	max_length = 0
+	min_length = 0
+	longer1024 = 0
 
-with gzip.open('/data/ms-marco/fulldocs.tsv.gz','rt') as f:
-	for line in f:
-		length = len(line)
-		if length < min_length: min_length = length
-		if length > max_length: max_length = length
-		if length > 1024: longer1024 = longer1024 + 1
+	with gzip.open('/data/ms-marco/fulldocs.tsv.gz','rt') as f:
+		for line in f:
+			length = len(line)
+			if length < min_length: min_length = length
+			if length > max_length: max_length = length
+			if length > 1024: longer1024 = longer1024 + 1
 
-print("TOTAL LINES %s" % (counter,))
-print("longer1024 %s" % (longer1024,))
-print("min_length %s" % (min_length,))
-print("max_length %s" % (max_length,))
+	logging.info("TOTAL LINES %s" % (counter,))
+	logging.info("longer1024 %s" % (longer1024,))
+	logging.info("min_length %s" % (min_length,))
+	logging.info("max_length %s" % (max_length,))
+except Exception as e:
+	logger.error(e)
