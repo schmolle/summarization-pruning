a33dec669c473b737d29983c34bfcfbd83ce3b98
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-04-22 10:37:55 +0200

    added inspect tool

---
+++ bin/tools/inspect_line_length.py
@@ -0,0 +1,14 @@
+import gzip
+
+counter = 0;
+max_length = 0;
+min_length = 0;
+longer1024 = 0;
+
+with gzip.open('/data/ms-marco/fulldocs.tsv.gz','rt') as f:
+	for line in f:
+		length = len(line)
+		if length < min_length: min_length = length
+		if length > max_length: max_length = length
+		if length > 1024: longer1024++;
+        counter++;
