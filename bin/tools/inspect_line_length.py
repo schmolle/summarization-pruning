e195dcb6b2c28570dba28c83b912621ff8e0854f
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-04-26 21:03:24 +0200

    added 4096 count

--- bin/tools/inspect_line_length.py
+++ bin/tools/inspect_line_length.py
@@ -12,6 +12,7 @@ try:
 	max_length = 0
 	min_length = 0
 	longer1024 = 0
+	longer4096 = 0
 
 	with gzip.open('/data/ms-marco/fulldocs.tsv.gz','rt') as f:
 		for line in f:
@@ -19,10 +20,12 @@ try:
 			if length < min_length: min_length = length
 			if length > max_length: max_length = length
 			if length > 1024: longer1024 = longer1024 + 1
+			if length > 4096: longer1024 = longer4096 + 1
 			counter = counter + 1
 
 	logging.info("TOTAL LINES %s" % (counter,))
 	logging.info("longer1024 %s" % (longer1024,))
+	logging.info("longer4096 %s" % (longer4096,))
 	logging.info("min_length %s" % (min_length,))
 	logging.info("max_length %s" % (max_length,))
 
