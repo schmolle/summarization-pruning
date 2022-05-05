5bdad989a45bf7379b3a537313e68a1c7a818a85
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-05-05 15:45:36 +0200

    added counter for 512 tokens

--- bin/tools/inspect_line_length.py
+++ bin/tools/inspect_line_length.py
@@ -7,6 +7,7 @@ logging.basicConfig(filename='data.log', level=logging.DEBUG)
 
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
+logging.info("############################################################################")
 logging.info("started run at: %s\n" % (current_time,))
 try:
 	counter = 0
@@ -14,6 +15,7 @@ try:
 	min_length = 0
 	longer1024 = 0
 	longer4096 = 0
+	longer512 = 0
 	t = PegasusTokenizerFast.from_pretrained("google/pegasus-xsum")
 	logging.info("I know {} tokens".format(len(t)))
 
@@ -24,10 +26,12 @@ try:
 			if length > max_length: max_length = length
 			if length > 1024: longer1024 = longer1024 + 1
 			if length > 4096: longer4096 = longer4096 + 1
+			if length > 512: longer512 = longer512 + 1
 			counter = counter + 1
 
 	logging.info("TOTAL LINES %s" % (counter,))
 	logging.info("longer1024 %s" % (longer1024,))
+	logging.info("longer512 %s" % (longer512,))
 	logging.info("longer4096 %s" % (longer4096,))
 	logging.info("min_length %s" % (min_length,))
 	logging.info("max_length %s" % (max_length,))
