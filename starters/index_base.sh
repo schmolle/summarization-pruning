026380da7911efdd511cf1d522dfac6c0b2fe19e
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-05-10 17:31:32 +0200

    changed anserini path

--- starters/index_base.sh
+++ starters/index_base.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sh target/appassembler/bin/IndexCollection -threads 1 -collection CleanTrecCollection \
+sh /home/jschmolzi/anserini/target/appassembler/bin/IndexCollection -threads 1 -collection CleanTrecCollection \
  -generator DefaultLuceneDocumentGenerator -input /data/ms-marco/fulldocs.tsv.gz \
  -index ~/indexes/msmarco/base -storePositions -storeDocvectors -storeRaw
