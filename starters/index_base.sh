354604d5ee87f8a48a7262694f27dfed3617c3fa
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-05-10 17:42:44 +0200

    added line ending

--- starters/index_base.sh
+++ starters/index_base.sh
@@ -3,3 +3,4 @@
 sh /home/jschmolzi/anserini/target/appassembler/bin/IndexCollection -threads 3 -collection CleanTrecCollection \
  -generator DefaultLuceneDocumentGenerator -input /data/ms-marco \
  -index ~/indexes/msmarco/base -storePositions -storeDocvectors -storeRaw
+ 
