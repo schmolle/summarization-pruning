69b14840c2c8fd91dbd37b089719c14a9cc643e3
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-05-11 10:12:49 +0200

    updated indexing starter

--- starters/index_base.sh
+++ starters/index_base.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sh /home/jschmolzi/anserini/target/appassembler/bin/IndexCollection -threads 3 -collection CleanTrecCollection \
- -generator DefaultLuceneDocumentGenerator -input /data/ms-marco \
+ -generator DefaultLuceneDocumentGenerator -input /home/jschmolzi/data/msmarco-doc \
  -index ~/indexes/msmarco/base -storePositions -storeDocvectors -storeRaw
  
