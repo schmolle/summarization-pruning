94ed81d417ecfd4d0cd87e48174b8b549bd5cd33
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-06-08 11:39:31 +0200

    added converter

---
+++ bin/tools/convert_trec_to_jsonl.py
@@ -0,0 +1,25 @@
+from html.parser import HTMLParser
+
+class TrecParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print("Encountered a start tag:", tag)
+
+    def handle_endtag(self, tag):
+        print("Encountered an end tag :", tag)
+
+    def handle_data(self, data):
+        print("Encountered some data  :", data)
+
+
+def convert_trec_to_jsonl(path):
+    counter = 0
+    with open(path, 'r') as f:
+        for line in f:
+            trec_parser = TrecParser()
+            trec_parser.feed(line)
+            counter = counter + 1
+            if counter > 34:
+                break
+            
+if __name__ == 'main':
+    convert_trec_to_jsonl('/home/jschmolzi/anserini/collections/msmarco-doc/msmarco-docs.trec.gz')
