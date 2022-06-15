e98a78d3281d9653bf9259858105e1f03bb0e86a
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-06-15 08:44:02 +0200

    save settings for indexing base collection

--- starters/index_base.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sh /home/jschmolzi/anserini/target/appassembler/bin/IndexCollection -threads 3 -collection CleanTrecCollection \
- -generator DefaultLuceneDocumentGenerator -input /home/jschmolzi/data/msmarco-doc \
- -index ~/indexes/msmarco/base -storePositions -storeDocvectors -storeRaw
- 
