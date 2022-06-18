30f71a67073c5499d80e7d434e17e5bf88c88918
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-06-18 12:52:53 +0200

    adding tests

--- bin/simple_summarizer.py
+++ bin/simple_summarizer.py
@@ -14,6 +14,8 @@ def sum_file(infile_path, outfile_path, device):
 		for line in infile:
 			if (counter % 10000) == 0:
 				logging.info("%d lines summarized" % (counter,))
+			if counter > 5:
+				break
 			counter = counter + 1
 			new_contents = longformer.summarize(line['contents'])
 			line['contents'] = new_contents[0]['summary_text']
