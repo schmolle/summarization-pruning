9744d167bab29b99fedf8e1c6cf7acb8cdc78643
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-06-22 15:35:46 +0200

    testing new output lengths

--- bin/test.py
+++ bin/test.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     txt = '''The tower is 324 metres (1,063 ft) tall, about the same height as an 81-storey building, and the tallest structure in Paris. Its base is square, measuring 125 metres (410 ft) on each side. During its construction, the Eiffel Tower surpassed the Washington Monument to become the tallest man-made structure in the world, a title it held for 41 years until the Chrysler Building in New York City was finished in 1930. It was the first structure to reach a height of 300 metres. Due to the addition of a broadcasting aerial at the top of the tower in 1957, it is now taller than the Chrysler Building by 5.2 metres (17 ft). Excluding transmitters, the Eiffel Tower is the second tallest free-standing structure in France after the Millau Viaduct.'''
     # init pegasus
     longformer = Longformer_Impl_With_Pipeline.LongformerWithPipeline(0)
-    for max, min in zip([100,50,30,20],[90,40,20,10]):
+    for max, min in zip([100,50,30,20],[80,30,10,0]):
         print(longformer.summarize(txt, max, min))
     print(longformer.summarize(txt, 0 , 0, False)[0]['summary_text'].split('\n')[0].strip())       
     # print(doBigBird(txt))
