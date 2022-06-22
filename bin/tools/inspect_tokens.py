214ce8bd94b80d5238e8eac936ae646d235f90f4
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-06-22 10:34:04 +0200

    testing with real tokenizer

--- bin/tools/inspect_tokens.py
+++ bin/tools/inspect_tokens.py
@@ -1,6 +1,7 @@
 import os
 import jsonlines
 
+tokenizer = LEDTokenizer.from_pretrained("allenai/led-large-16384-arxiv")
 base_path = '/home/jschmolzi/anserini/collections/msmarco-doc-json-base'
 max_tokens = 16384 - 50
 too_long_counter_all = 0
@@ -8,8 +9,9 @@ for filename in os.listdir(base_path):
     with jsonlines.open(os.path.join(base_path, filename), 'r') as infile:
         for line in infile:
             too_long_counter = 0
-            line_arr = line['contents'].split()
-            line_length = len(line_arr)
+            line = line['contents']
+            input_ids = tokenizer(txt, return_tensors="pt").input_ids.to("cuda")
+            line_length = len(input_ids)
             if line_length > max_tokens:
                 too_long_counter = too_long_counter + 1
         too_long_counter_all = too_long_counter_all + too_long_counter
