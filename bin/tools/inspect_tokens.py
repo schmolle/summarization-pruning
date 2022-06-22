e669ab3ad49dbd0e93cb8462a6d4d84292570a4c
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-06-22 10:17:12 +0200

    bugfix

--- bin/tools/inspect_tokens.py
+++ bin/tools/inspect_tokens.py
@@ -5,7 +5,7 @@ base_path = '/home/jschmolzi/anserini/collections/msmarco-doc-json-base'
 max_tokens = 16384 - 50
 too_long_counter_all = 0
 for filename in os.listdir(base_path):
-    with jsonlines.open(filename, 'r') as infile:
+    with jsonlines.open(os.path.join(base_path, filename), 'r') as infile:
         for line in infile:
             too_long_counter = 0
             line_arr = line.split()
@@ -13,5 +13,5 @@ for filename in os.listdir(base_path):
             if line_length > max_tokens:
                 too_long_counter = too_long_counter + 1
         too_long_counter_all = too_long_counter_all + too_long_counter
-        print('File "%s" has %d too long lines' %s (base_path.split[-1], ))
+        print('File "%s" has %d too long lines' %s (filename, ))
 print('%d too long files total' % (too_long_counter_all,))
