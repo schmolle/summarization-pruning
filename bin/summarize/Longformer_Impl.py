0756623301f7c09b8d40c8ca8fedd93a909593b1
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-06-24 11:08:00 +0200

    using cuda for tokenizer

--- bin/summarize/Longformer_Impl.py
+++ bin/summarize/Longformer_Impl.py
@@ -5,18 +5,18 @@ import torch
 class Longformer():
 	
 	def __init__(self):
-		self._tokenizer = LEDTokenizer.from_pretrained("allenai/led-large-16384-arxiv")
+		self._tokenizer = LEDTokenizer.from_pretrained("allenai/led-large-16384-arxiv").to("cuda")
 		self._model = LEDForConditionalGeneration.from_pretrained("allenai/led-large-16384-arxiv", return_dict_in_generate=True).to("cuda")
 		
 	def summarize(self, txt):
 
-		input_ids = self._tokenizer(txt, return_tensors="pt").input_ids.to("cuda")
+		input_ids = self._tokenizer(txt, return_tensors="pt").input_ids
 		global_attention_mask = torch.zeros_like(input_ids)
 		# set global_attention_mask on first token
 		global_attention_mask[:, 0] = 1
 				
 		sequences = self._model.generate(input_ids, global_attention_mask=global_attention_mask).sequences
 		
-		summary = self._tokenizer.batch_decode(sequences).to("cuda")
+		summary = self._tokenizer.batch_decode(sequences)
 		
 		return summary
