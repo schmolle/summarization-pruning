0327344c42e5579e3d45a8bf60c895da972ff584
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-06-27 21:05:57 +0200

    bugfix

--- bin/dataset_summarizer.py
+++ bin/dataset_summarizer.py
@@ -29,7 +29,7 @@ def sum_file(infile_path, outfile_path, device):
 	dataset = load_dataset('json', data_files=infile_path)	
 	dataset = dataset['train'].select(range(100))
 	
-	dataset = dataset.filter(lambda input: filter_by_token_length(input, idx, longformer_tokenizer))
+	dataset = dataset.filter(lambda input: filter_by_token_length(input, longformer_tokenizer))
 	dataset = dataset.select(range(15))
 	
 	dataset = dataset.map(lambda input, idx: map_to_summary(input, idx, longformer_pipeline), with_indices=True)
