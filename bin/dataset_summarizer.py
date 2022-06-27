abf54fb80020edc624583b9e992ab3689f5f9edf
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-06-27 19:19:08 +0200

    revert

--- bin/dataset_summarizer.py
+++ bin/dataset_summarizer.py
@@ -18,7 +18,7 @@ def sum_file(infile_path, outfile_path, device):
 	longformer_tokenizer = LEDTokenizer.from_pretrained("allenai/led-large-16384-arxiv")
 	dataset = load_dataset('json', data_files=infile_path)
 	print(dataset)
-	dataset = dataset[:4]
+	dataset = dataset['train'][:4]
 	print(dataset)
 	print(len(dataset))
 	dataset = dataset.filter(filter_by_token_length)
