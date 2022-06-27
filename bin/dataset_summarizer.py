4e0160ab231b5a8167fc6bf6d18e680472e4b59d
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-06-27 19:34:04 +0200

    trying to call tokenizer

--- bin/dataset_summarizer.py
+++ bin/dataset_summarizer.py
@@ -5,21 +5,22 @@ import sys
 from transformers import LEDTokenizer
 from datasets import load_dataset
 
-longformer_tokenizer = LEDTokenizer.from_pretrained("allenai/led-large-16384-arxiv")
 
 
-def filter_by_token_length(input):
+def filter_by_token_length(input, tokenizer):
 	max_tokens = 16384
 	print("INPUT")
 	print(input)
 	txt = input['contents']
-	tokens = longformer_tokenizer(txt, return_tensors="pt").input_ids[0]
+	tokens = tokenizer(txt, return_tensors="pt").input_ids[0]
 	token_length = len(tokens)
 	print(token_length)
 	return token_length <= max_tokens
 	
 def sum_file(infile_path, outfile_path, device):
 	# longformer_pipeline = Longformer_Impl_With_Pipeline.LongformerWithPipeline(device)
+	longformer_tokenizer = LEDTokenizer.from_pretrained("allenai/led-large-16384-arxiv")
+	
 	dataset = load_dataset('json', data_files=infile_path)
 	print(dataset)
 	
@@ -31,7 +32,7 @@ def sum_file(infile_path, outfile_path, device):
 	
 	print(len(dataset))
 	
-	dataset = dataset.filter(filter_by_token_length)
+	dataset = dataset.filter(filter_by_token_length, longformer_tokenizer)
 	print(len(dataset))
 			
 if __name__ == "__main__":
