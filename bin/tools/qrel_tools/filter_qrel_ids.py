70d9f3c3eb89c25b3f9b2ab3f2e1453d5854fab3
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-06-30 17:15:23 +0200

    removing test boundaries

--- bin/tools/qrel_tools/filter_qrel_ids.py
+++ bin/tools/qrel_tools/filter_qrel_ids.py
@@ -4,11 +4,8 @@ def filter_qrel_ids(inpath, outpath):
     with open(inpath, 'r') as infile, open(outpath, 'w') as outfile:
         for line in infile:
             line_array = line.split('\t')
-            print(line_array)
             qrel_id = line_array[2]
-            print(qrel_id)
             outfile.write(qrel_id + '\n')
-            break
 
 if __name__ == "__main__":
     inpath = '/home/jschmolzi/anserini/src/main/resources/topics-and-qrels/qrels.msmarco-doc.dev.txt'
