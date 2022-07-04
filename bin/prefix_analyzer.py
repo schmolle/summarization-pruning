cef15bbbbbe317429d2cc224c6f28e7f38a1db08
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-07-04 16:10:31 +0200

    using multiprocessing for filter method

--- bin/prefix_analyzer.py
+++ bin/prefix_analyzer.py
@@ -1,3 +1,4 @@
+from summarize import Longformer_Impl_With_Pipeline
 import logging
 import os
 import sys
@@ -12,15 +13,38 @@ def filter_by_token_length(input, tokenizer):
 	tokens = tokenizer(txt, return_tensors="pt").input_ids[0]
 	token_length = len(tokens)
 	return (token_length <= max_tokens) and (token_length >= min_tokens)
+
+def summarize_without_map(input, idx, pipeline):
+	if idx % 1000 == 0:
+		logging.info("%d summarized" % (idx,))
+	summarized_array = []
+	
+	new_contents = pipeline.summarize(input['contents'], 50, 30)
+	summarized_array[0] = new_contents[0]['summary_text'].split('\n')
+	
+	new_contents = pipeline.summarize(input['contents'], 100, 80)
+	summarized_array[1] = new_contents[0]['summary_text'].split('\n')
+	
+	new_contents = pipeline.summarize(input['contents'], 150, 170)
+	summarized_array[2] = new_contents[0]['summary_text'].split('\n')
+	
+	if (summarized_array[1].startswith(summarized_array[0])) and 
+	   (summarized_array[2].startswith(summarized_array[0])) and 
+	   (summarized_array[2].startswith(summarized_array[1])):
+		logging.log('ID %d true' % (idx,))
+	else:
+		logging.log('ID %d false' % (idx,))
+	return true
 	
 def sum_file(infile_path, outfile_path, device):
+	longformer_pipeline = Longformer_Impl_With_Pipeline.LongformerWithPipeline(device)
 	longformer_tokenizer = LEDTokenizer.from_pretrained("allenai/led-large-16384-arxiv")
 	
 	dataset = load_dataset('json', data_files=infile_path)	
 	dataset = dataset['train']
 	
 	logging.info("Filtering...")
-	dataset = dataset.filter(lambda input: filter_by_token_length(input, longformer_tokenizer))
+	dataset = dataset.filter(lambda input: filter_by_token_length(input, longformer_tokenizer, num_proc=30))
 	print(dataset)
 	dataset = dataset.shuffle(seed=42).select(range(20000))
 	print(dataset)
