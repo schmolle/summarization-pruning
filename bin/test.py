425341cafe9e5290293d5674728b850005ea6060
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-07-04 18:05:29 +0200

    bugfix

--- bin/test.py
+++ bin/test.py
@@ -2,6 +2,5 @@ from datasets import load_dataset
 
 if __name__ == '__main__':
     infile_path = '/home/jschmolzi/anserini/collections/msmarco-doc-json-base/docs00aa.json'
-    dataset = load_dataset('json', data_files=infile_path, split='train[:100]')    
-    dataset = dataset['train']
+    dataset = load_dataset('json', data_files=infile_path, split='train[:100]')
     print(dataset)
