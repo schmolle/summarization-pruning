4ce6a836aa04bbbffc15a4cbd9c20f59ced55762
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-07-04 18:06:23 +0200

    adding column

--- bin/test.py
+++ bin/test.py
@@ -4,3 +4,9 @@ if __name__ == '__main__':
     infile_path = '/home/jschmolzi/anserini/collections/msmarco-doc-json-base/docs00aa.json'
     dataset = load_dataset('json', data_files=infile_path, split='train[:100]')
     print(dataset)
+    
+    new_column = [False] * len(dataset)
+    dataset = dataset.add_column("is_prefix", new_column)
+    print(dataset)
+    
+    print(dataset['is_prefix'])
