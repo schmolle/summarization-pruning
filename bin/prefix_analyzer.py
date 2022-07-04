71ea6f11320be2e73782f9835edc042df125b69c
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-07-04 18:17:59 +0200

    removing sleep

--- bin/prefix_analyzer.py
+++ bin/prefix_analyzer.py
@@ -41,13 +41,10 @@ def sum_file(infile_path, outfile_path, device):
 	
 	logging.info("Filtering...")
 	dataset = dataset.filter(lambda input: filter_by_token_length(input, longformer_tokenizer), num_proc=30)
-	time.sleep(20)
-	print(dataset)
 	dataset = dataset.shuffle(seed=42).select(range(20000))
 	
 	new_column = [False] * len(dataset)
 	dataset = dataset.add_column("is_prefix", new_column)
-	print(dataset)
 	logging.info("DONE!!!")
 			
 if __name__ == "__main__":
