a75cd94956a90955543b22e152330789cf3de375
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-07-06 10:17:34 +0200

    fixing imports

--- bin/tools/extract_relevant_docs.py
+++ bin/tools/extract_relevant_docs.py
@@ -1,5 +1,5 @@
-from qrel_tools.qrel_defs import qrel_json_file_path
-from qrel_tools.read_qrels import read_qrels
+from tools.qrel_tools.qrel_defs import qrel_json_file_path
+from tools.qrel_tools.read_qrels import read_qrels
 
 if __name__ == '__main__':
     indir = '/home/jschmolzi/anserini/collections/base'
