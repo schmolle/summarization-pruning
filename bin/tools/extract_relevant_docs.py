7134345d5babc29ab36b06b9d938a479ffdc0e58
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-07-06 09:52:47 +0200

    adding tool to extract relevant files from base files

---
+++ bin/tools/extract_relevant_docs.py
@@ -0,0 +1,9 @@
+from qrel_tools.qrel_defs import qrel_json_file_path
+from qrel_tools.read_qrels import read_qrels
+
+if __name__ == '__main__':
+    indir = '/home/jschmolzi/anserini/collections/base'
+    print(os.listdir(indir))
+    # qrel_arr = read_qrels()
+    
+    
--- bin/tools/qrel_tools/qrel_defs.py
+++ bin/tools/qrel_tools/qrel_defs.py
@@ -1,1 +1,2 @@
 qrel_file_path = '/home/jschmolzi/data/relevant_docs'
+qrel_json_file_path = '/home/jschmolzi/anserini/collections/base/relevant.json'
