7179b7efaa894f5050ccf19b10aec20ede6e9acb
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-07-06 10:35:28 +0200

    adding output and counter

--- bin/tools/qrel_tools/extract_relevant_docs.py
+++ bin/tools/qrel_tools/extract_relevant_docs.py
@@ -2,6 +2,7 @@ from qrel_defs import qrel_json_file_path
 from read_qrels import read_qrels
 import os
 import jsonlines
+import json
 
 
 if __name__ == '__main__':
@@ -9,13 +10,17 @@ if __name__ == '__main__':
     relevant_path = 'relevant.json'
     outfile = open(relevant_path, 'w')
     qrel_arr = read_qrels()
+    counter = 0
     for infile_path in os.listdir(indir):
         if infile_path != 'relevant.json':
             infile_path = os.path.join(indir, infile_path)
             with jsonlines.open(infile_path, 'r') as infile:
                 for line in infile:
                     id = line['id']
-                    print(id)
+                    if id in qrel_arr:
+                        outfile.write(json.dumps(line) + '\n')
+                        counter = counter + 1
     outfile.close()
+    print("%d relevant docs found" % (counter,))
     
     
