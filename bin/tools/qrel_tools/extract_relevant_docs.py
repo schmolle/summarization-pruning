0552c460eb6b854e4a5997a1fc552e787188a42d
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-07-06 10:32:59 +0200

    reading ids

--- bin/tools/qrel_tools/extract_relevant_docs.py
+++ bin/tools/qrel_tools/extract_relevant_docs.py
@@ -1,13 +1,20 @@
 from qrel_defs import qrel_json_file_path
 from read_qrels import read_qrels
 import os
+import jsonlines
 
 
 if __name__ == '__main__':
     indir = '/home/jschmolzi/anserini/collections/base'
+    relevant_path = 'relevant.json'
+    outfile = open(relevant_path, 'w')
+    qrel_arr = read_qrels()
     for infile_path in os.listdir(indir):
         if infile_path != 'relevant.json':
-            print(os.path.join(indir, infile_path))
-    # qrel_arr = read_qrels()
+            infile_path = os.path.join(indir, infile_path)
+            with jsonlines.open(infile_path, 'r') as infile:
+                for line in infile:
+                    id = line['id']
+    outfile.close()
     
     
