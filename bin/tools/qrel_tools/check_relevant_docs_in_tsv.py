a6975e8f56bcdc2ead72455d4dc016e61651d721
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-07-07 15:45:18 +0200

    removing print

--- bin/tools/qrel_tools/check_relevant_docs_in_tsv.py
+++ bin/tools/qrel_tools/check_relevant_docs_in_tsv.py
@@ -9,7 +9,6 @@ if __name__ == '__main__':
         for line in infile:
             id, url, title, body = line.split('\t')
             if id in qrel_arr:
-                outfile.write(json.dumps(line) + '\n')
                 counter = counter + 1
                 if counter % 200 == 0:
                     print("%d relevant docs found" % (counter,))
