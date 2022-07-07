9ca515483ba109b67f6510076ba9af56ad3ecf17
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-07-07 15:44:12 +0200

    checking for qrels in original doc

---
+++ bin/tools/qrel_tools/check_relevant_docs_in_tsv.py
@@ -0,0 +1,18 @@
+import os
+from read_qrels import read_qrels
+
+if __name__ == '__main__':
+    infile_path = '/home/jschmolzi/anserini/collections/msmarco-doc/msmarco-docs.tsv'
+    qrel_arr = read_qrels()
+    counter = 0
+    with open(infile_path, 'r') as infile:
+        for line in infile:
+            id, url, title, body = line.split('\t')
+            if id in qrel_arr:
+                outfile.write(json.dumps(line) + '\n')
+                counter = counter + 1
+                if counter % 200 == 0:
+                    print("%d relevant docs found" % (counter,))
+    print("%d relevant docs found" % (counter,))
+    
+    
