16765ba76f5585d7f2f39b3978a7254e851030e3
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-07-15 09:10:49 +0200

    removing specific length arg from longformer

--- bin/summarize/Longformer_Impl_With_Pipeline.py
+++ bin/summarize/Longformer_Impl_With_Pipeline.py
@@ -7,8 +7,6 @@ class LongformerWithPipeline():
         model_name = "allenai/led-large-16384-arxiv"
         self._Longformer = pipeline("summarization", model=model_name, device=device)  
 
-    def summarize(self, txt, max_length=50, min_length=30, specific_length=True):
-        if specific_length:
-            return self._Longformer(txt, max_length=max_length, min_length=min_length, do_sample=False, truncation=True)
-        else:
-            return self._Longformer(txt, do_sample=False)
+    def summarize(self, txt, max_length=50, min_length=30):
+        return self._Longformer(txt, max_length=max_length, min_length=min_length, do_sample=False, truncation=True)
+    
