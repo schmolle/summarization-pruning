6c7d453e795924d112ddbc87e7cd6ecc295ae7d3
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-07-15 14:15:40 +0200

    adding script to summarize qrels with bart

---
+++ bin/bart_qrel_summarizer.py
@@ -0,0 +1,81 @@
+from summarize import Bart_Impl_With_Pipeline
+import logging
+import os
+import sys
+from transformers import BartTokenizer
+from datasets import load_dataset
+
+
+def map_to_summary(input, idx, pipeline):
+	max_tokens = 1024
+	token_length = min(max_tokens, input['token_length'])
+	sumarize_length = token_length // 2
+	sumarize_length = min(512, sumarize_length)
+	if idx % 1000 == 0:
+		logging.info("%d summarized" % (idx,))
+	try:
+		if sumarize_length > 5:
+			new_contents = pipeline.summarize(input['contents'], sumarize_length, sumarize_length)
+			input['contents'] = new_contents[0]['summary_text']
+		else:
+			logging.info('token_length: %s' % (token_length,))
+			logging.info('contents: %s' % (input['contents'],))
+			logging.info('id: %s' % (input['id'],))
+	except Exception as e:
+		logging.error(e)
+		logging.info('token_length: %s' % (token_length,))
+		logging.info('contents: %s' % (input['contents'],))
+		logging.info('id: %s' % (input['id'],))
+		raise e	
+	return input
+	
+def add_token_length(input, tokenizer):
+	txt = input['contents']
+	tokens = tokenizer(txt, return_tensors="pt").input_ids[0]
+	token_length = len(tokens)
+	input['token_length'] = token_length
+	return input
+	
+def sum_file(infile_path, outfile_path, device):
+	longformer_pipeline = Bart_Impl_With_Pipeline.LongformerWithPipeline(device)
+	longformer_tokenizer = BartTokenizer.from_pretrained("facebook/bart-large-cnn")
+	
+	# Loading dataset
+	dataset = load_dataset('json', data_files=infile_path)	
+	dataset = dataset['train']
+	
+	# adding column for token length
+	new_column = [0] * len(dataset)
+	dataset = dataset.add_column("token_length", new_column)
+	
+	logging.info("Counting Tokens...")
+	dataset = dataset.map(lambda input: add_token_length(input, longformer_tokenizer))
+	
+	logging.info("Summarizing...")
+	dataset = dataset.map(lambda input, idx: map_to_summary(input, idx, longformer_pipeline), with_indices=True)
+	
+	logging.info("Storing...")
+	dataset.to_json(outfile_path)
+	logging.info("DONE!!!")
+			
+if __name__ == "__main__":
+
+	infile_path = '/home/jschmolzi/anserini/collections/qrels/relevant.json'
+	outfile_path = '/home/jschmolzi/anserini/collections/qrels/bart_qrels_base.json'
+	logfile = '/home/jschmolzi/logs/bart.log'
+	device = 1
+	
+	if os.path.isfile(logfile):
+		with open(logfile, 'w') as f:
+			pass
+	logging.basicConfig(format='%(asctime)s %(message)s', filename=logfile, level=logging.DEBUG)
+	print(f'Logging to file {logfile}')
+	print(f'Writing to file {outfile_path}')
+	
+	logging.info('Run Started')
+	try:
+		sum_file(infile_path, outfile_path, device)
+	except Exception as e:
+		logging.error(e)		
+		
+	# logging.info('Run Finished')
