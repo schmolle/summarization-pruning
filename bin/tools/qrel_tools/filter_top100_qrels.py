7ccf55f365472cae1f32104ae615659d423f9228
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-07-21 15:21:21 +0200

    adding prints

--- bin/tools/qrel_tools/filter_top100_qrels.py
+++ bin/tools/qrel_tools/filter_top100_qrels.py
@@ -4,9 +4,13 @@ def filter_top100_qrels(inpath):
     qrels = set()
     with open(inpath, 'r') as infile:
         for line in infile:
+            print(line)
             line_array = line.split(' ')
+            print(line_array)
             dok_id = line_array[2]
+            print(dok_id)
             dok_rank = line_array[3]
+            print(dok_rank)
             qrels.add(dok_id)
             break
     print(qrels)
