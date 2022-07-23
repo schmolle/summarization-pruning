acb92371e1224be389fc41964717cd10d0e2bc75
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-07-23 19:55:36 +0200

    adding print

--- bin/tools/qrel_tools/filter_top100_qrels.py
+++ bin/tools/qrel_tools/filter_top100_qrels.py
@@ -5,12 +5,14 @@ def filter_top100_qrels(inpath):
     top10_qrels = set()
     top1_qrels = set()
     counter = 0
+    counter2 = 0
     with open(inpath, 'r') as infile:
         for line in infile:
             line_array = line.split(' ')
             dok_rank = line_array[3]
             dok_rank = int(dok_rank)
             if dok_rank <= 100:
+                counter2 = counter2 + 1
                 dok_id = line_array[2]
                 top10_qrels.add(dok_id)
                 if dok_rank == 1:
@@ -19,6 +21,7 @@ def filter_top100_qrels(inpath):
     print("top1  qrels: %s" % (len(top1_qrels),))           
     print("top10 qrels: %s" % (len(top10_qrels),))
     print(counter)
+    print(counter2)
     old_qrels = read_qrels()
     old_qrels = set(old_qrels)
     diff = top10_qrels.intersection(old_qrels)
