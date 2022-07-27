f076bd6662a46ec0a7f1a88dd127b03981cc4c7e
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-07-27 14:30:22 +0200

    adding tool to analyze qrels

---
+++ bin/tools/qrel_tools/qrel_analyzer.py
@@ -0,0 +1,14 @@
+import sys
+from transformers import LEDTokenizer
+from datasets import load_dataset
+
+def extract_lengths():
+    infile_path = '/home/jschmolzi/anserini/collections/qrels/relevant.json'
+    dataset = load_dataset('json', data_files=infile_path)    
+    dataset = dataset['train']
+    print(dataset)
+    
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        if sys.argv[1] == 'e':
+            extract_lengths() 
