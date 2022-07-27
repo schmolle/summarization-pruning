b536dfdd6adf18bb655dd5401a619ccd1dd82f65
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-07-27 19:37:24 +0200

    adding plots

--- bin/tools/qrel_tools/plot_qrels.py
+++ bin/tools/qrel_tools/plot_qrels.py
@@ -1,9 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
+if os.name == 'nt':
+    plot_dir = r'C:\Users\User\git\summarization-pruning\data\plots'
+else:
+    plot_dir = r'/home/jschmolzi/summarization-pruning/data/plots'
+    
+plot_path = os.path.join(plot_dir, 'plot.png')
 x = np.linspace(0, 10, 100)
 
 plt.plot(x, np.sin(x))
 plt.plot(x, np.cos(x))
 
-plt.show()
+plt.savefig(plot_path)
