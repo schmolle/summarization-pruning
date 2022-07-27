98a23313149e9ec2b9be6723c4092f4f7b7df315
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-07-27 14:31:06 +0200

    only use contents

--- bin/tools/qrel_tools/qrel_analyzer.py
+++ bin/tools/qrel_tools/qrel_analyzer.py
@@ -6,7 +6,7 @@ def extract_lengths():
     infile_path = '/home/jschmolzi/anserini/collections/qrels/relevant.json'
     dataset = load_dataset('json', data_files=infile_path)    
     dataset = dataset['train']
-    print(dataset)
+    print(dataset['contents'])
     
 if __name__ == "__main__":
     if len(sys.argv) > 1:
