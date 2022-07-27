80d80754c80688e7f8a5c66cdc2d4f8196bfb64c
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-07-27 10:12:37 +0200

    adding tools to read/extract/delete top10 qrels

---
+++ bin/tools/qrel_tools/extract_10relevant_docs.py
@@ -0,0 +1,28 @@
+from qrel_defs import qrel_json_file_path
+from read_10qrels import read_10qrels
+import os
+import jsonlines
+import json
+
+
+if __name__ == '__main__':
+    indir = '/home/jschmolzi/anserini/collections/base'
+    relevant_path = 'relevant.json'
+    outfile = open(os.path.join(indir, relevant_path), 'w', encoding='utf-8')
+    qrel_arr = read_10qrels()
+    counter = 0
+    for infile_path in os.listdir(indir):
+        if infile_path != 'relevant.json':
+            infile_path = os.path.join(indir, infile_path)
+            with jsonlines.open(infile_path, 'r', encoding='utf-8') as infile:
+                for line in infile:
+                    id = line['id']
+                    if id in qrel_arr:
+                        outfile.write(json.dumps(line) + '\n')
+                        counter = counter + 1
+                        if counter % 200 == 0:
+                            print("%d relevant docs found" % (counter,))
+    outfile.close()
+    print("%d relevant docs found" % (counter,))
+    
+    
---
+++ bin/tools/qrel_tools/read_10qrels.py
@@ -0,0 +1,17 @@
+from qrel_defs import qrel10_file_path
+
+def read_qrels():
+    inpath = qrel_file_path
+    with open(qrel_file_path, 'r') as infile:
+        qrels = []
+        for line in infile:
+            qrels.append(line.strip())
+        return qrels
+    
+    
+if __name__ == '__main__':
+    out_arr = read_qrels()
+    print(out_arr[0])
+    print(out_arr[1])
+    print(out_arr[-1])
+    print(len(out_arr))
