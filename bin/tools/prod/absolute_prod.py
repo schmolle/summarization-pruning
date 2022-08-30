d84b9a49bb6b88a89c3e78e0fd3a82787117ff98
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-08-30 10:59:59 +0200

    bugfix

--- bin/tools/prod/absolute_prod.py
+++ bin/tools/prod/absolute_prod.py
@@ -1,6 +1,6 @@
 from prod_defs import *
 import sys
-if __name == "__main__":
+if __name__ == "__main__":
     usage_string = 'Pass 1 Argument: nr of Tokens [1,512]'
     if len(sys.args) != 2:
         print(usage_string)
