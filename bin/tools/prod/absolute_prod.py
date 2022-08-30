b24739b531cb375fe9740c7378c975a7ed3c5dc0
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-08-30 11:10:32 +0200

    bugfix

--- bin/tools/prod/absolute_prod.py
+++ bin/tools/prod/absolute_prod.py
@@ -1,18 +1,35 @@
 from prod_defs import *
 import sys
 
-def absolute_prod(tokens):
+def absolute_prod(tokens, model):
     print("calling with %d tokens" % (tokens,))
+    if model == 'bart':
+        inpath = bart_path
+    elif model == 'pegasus':
+        inpath = pegasus_path
+    else:
+        inpath = longformer_path
+    
+    print(inpath)
+    
+    
+    
+    
+    
 if __name__ == "__main__":
-    usage_string = 'Pass 1 Argument: nr of Tokens [1,512]'
-    if len(sys.argv) != 2:
+    usage_string = 'Pass 2 Arguments: NrOfTokens[1,512] Model["bart", "pegasus", "longformer"]'
+    if len(sys.argv) != 3:
         print(usage_string)
     try:
         tokens = int(sys.argv[1])
+        model = sys.argv[2].lower()
     except Exception as e:
         print(usage_string, e)
     if 1 <= tokens <= 512:
-        absolute_prod(tokens)
+        if model not in ["bart", "pegasus", "longformer"]:
+            print(usage_string)
+        else:
+            absolute_prod(tokens, model)
     else:
         print(usage_string)
     
