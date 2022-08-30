8b61f378cd70bf941856ac7504d2b9d0621e4568
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-08-30 10:57:30 +0200

    adding tool to move relevant docs to prod

---
+++ bin/tools/prod/absolute_prod.py
@@ -0,0 +1,15 @@
+from prod_def import *
+import sys
+if __name == "__main__":
+    usage_string = 'Pass 1 Argument: nr of Tokens [1,512]'
+    if len(sys.args) != 2:
+        print(usage_string)
+    try:
+        tokens = int(sys.args[1])
+    except Exception as e:
+        print(usage_string, e)
+    if 1 <= tokens <= 512:
+        absolute_prod(tokens)
+    else:
+        print(usage_string)
+    
