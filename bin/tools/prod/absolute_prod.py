798c42a2b563102a2ae2645d58c645dde4132ab5
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-08-30 11:00:24 +0200

    bugfix

--- bin/tools/prod/absolute_prod.py
+++ bin/tools/prod/absolute_prod.py
@@ -2,10 +2,10 @@ from prod_defs import *
 import sys
 if __name__ == "__main__":
     usage_string = 'Pass 1 Argument: nr of Tokens [1,512]'
-    if len(sys.args) != 2:
+    if len(sys.argv) != 2:
         print(usage_string)
     try:
-        tokens = int(sys.args[1])
+        tokens = int(sys.argv[1])
     except Exception as e:
         print(usage_string, e)
     if 1 <= tokens <= 512:
