f115559222dbcdc7c38a7f98385acac5357abf8d
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-08-30 19:29:52 +0200

    adding tool to choose percent token lengths

---
+++ bin/tools/prod/percent_prod.py
@@ -0,0 +1,51 @@
+from prod_defs import *
+import sys
+import jsonlines
+import json
+
+def absolute_prod(tokens, model):
+    print("calling with %d tokens" % (tokens,))
+    if model == 'bart':
+        inpath = bart_path
+        max_tokens = 512
+    elif model == 'pegasus':
+        inpath = pegasus_path
+        max_tokens = 256
+    else:
+        inpath = longformer_path
+        max_tokens = 512
+    
+    print("infile:", inpath)
+    outpath = destination_path
+    print("outfile:", outpath)
+    outfile = open(outpath, 'w', encoding='utf-8')
+    with jsonlines.open(inpath, 'r') as infile:
+        for line in infile:
+            split_contentes = line['contents'].split()
+            tokens = math.ceil(max_tokens * tokens)
+            if len(split_contentes) < tokens:
+                continue
+            else:
+                line['contents'] = (' ').join(split_contentes[0:tokens])
+                outfile.write(json.dumps(line) + '\n')
+            break
+    outfile.close()
+    
+    
+if __name__ == "__main__":
+    usage_string = 'Pass 2 Arguments: NrOfTokens[1,512] Model["bart", "pegasus", "longformer"]'
+    if len(sys.argv) != 3:
+        print(usage_string)
+    try:
+        tokens = float(sys.argv[1])
+        model = sys.argv[2].lower()
+    except Exception as e:
+        print(usage_string, e)
+    if 0.1 <= tokens <= 0.9:
+        if model not in ["bart", "pegasus", "longformer"]:
+            print(usage_string)
+        else:
+            absolute_prod(tokens, model)
+    else:
+        print(usage_string)
+    
