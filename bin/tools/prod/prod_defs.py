e7dcbb12a7f3f3bd1446e4ac199cbdab12756510
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-08-30 10:59:18 +0200

    bugfix

--- bin/tools/prod/prod_defs.py
+++ bin/tools/prod/prod_defs.py
@@ -1,5 +1,5 @@
 collection_path = r'/home/jschmolzi/anserini/collections/base'
-delete_relevant_path = r'/home/jschmolzi/anserini/collections/base/relevant.json'
+destination_path = r'/home/jschmolzi/anserini/collections/base/relevant.json'
 bart_path = r'/home/jschmolzi/anserini/collections/qrels/bart10_qrels_base.json'
 pegasus_path = r'/home/jschmolzi/anserini/collections/qrels/Pegasus10_qrels_base.json'
 longformer_path = r'/home/jschmolzi/anserini/collections/qrels/longformer10_qrels_base.json'
