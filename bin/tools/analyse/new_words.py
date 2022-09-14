119a1537be787dbed2d46b337098d0d600255fb1
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-09-14 09:32:01 +0200

    adding tool to count number of new words

---
+++ bin/tools/analyse/new_words.py
@@ -0,0 +1,56 @@
+import jsonlines
+import os
+
+BASE_PATH = r'/home/jschmolzi/anserini/collections/qrels'
+full_len = 51465
+long = os.path.join(BASE_PATH, 'longformer10_qrels_base.json')
+bart = os.path.join(BASE_PATH, 'bart10_qrels_base.json')
+pegasus = os.path.join(BASE_PATH, 'Pegasus10_qrels_base.json')
+base = os.path.join(BASE_PATH, '10_relevant.json')
+
+counter_bart = 0
+counter_long = 0
+counter_pegasus = 0
+
+arr_new_words_bart = []
+arr_new_words_long = []
+arr_new_words_pegasus = []
+
+with jsonlines.open(base, 'r') as f_base, \
+        jsonlines.open(bart, 'r') as f_bart,  \
+        jsonlines.open(pegasus, 'r') as f_pegasus, \
+        jsonlines.open(long, 'r') as f_long:
+    for line_base, line_bart, line_pegasus, line_long in zip(f_base, f_bart, f_pegasus, f_long):
+        new_words_bart = 0
+        new_words_long = 0
+        new_words_pegasus = 0
+        base = line_base['contents'].split()
+        bart = line_bart['contents'].split()
+        pegasus = line_pegasus['contents'].split()
+        long = line_long['contents'].split()
+        
+        for word in pegasus:
+            if word not in base:
+                new_words_pegasus = new_words_pegasus + 1  
+        if new_words_pegasus > 0:
+            counter_pegasus = counter_pegasus + 1
+            arr_new_words_pegasus.append(new_words_pegasus)
+                
+        for word in bart:
+            if word not in base:
+                new_words_bart = new_words_bart + 1  
+        if new_words_bart > 0:
+            counter_bart = counter_bart + 1
+            arr_new_words_bart.append(new_words_bart)
+                
+        for word in long:
+            if word not in base:
+                new_words_long = new_words_long + 1  
+        if new_words_long > 0:
+            counter_long = counter_long + 1
+            arr_new_words_long.append(counter_long)
+    
+    
+    print("counter_bart", counter_bart)
+    print("counter_pegasus", counter_pegasus)
+    print("counter_long", counter_long)
