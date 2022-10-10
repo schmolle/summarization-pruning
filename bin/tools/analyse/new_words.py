a3d3b2eed114089a5743a5b332b1b640af29e3b2
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-10-10 09:58:08 +0200

    getting id

--- bin/tools/analyse/new_words.py
+++ bin/tools/analyse/new_words.py
@@ -31,6 +31,13 @@ counter_pegasus = 0
 arr_new_words_bart = []
 arr_new_words_long = []
 arr_new_words_pegasus = []
+max_id_bart = ''
+max_value_bart = 0
+max_id_long = ''
+max_value_long = 0
+max_id_pegasus = ''
+max_value_pegasus = 0
+
 
 with jsonlines.open(base, 'r') as f_base, \
         jsonlines.open(bart, 'r') as f_bart,  \
@@ -64,13 +71,19 @@ with jsonlines.open(base, 'r') as f_base, \
         if new_words_pegasus > 0:
             counter_pegasus = counter_pegasus + 1
         arr_new_words_pegasus.append(new_words_pegasus)
-                
+        if new_words_pegasus > max_value_pegasus:
+            max_id_pegasus = line_pegasus['id']
+            max_value_pegasus = new_words_pegasus     
+            
         for word in bart:
             if word not in base:
                 new_words_bart = new_words_bart + 1  
         if new_words_bart > 0:
             counter_bart = counter_bart + 1
         arr_new_words_bart.append(new_words_bart)
+        if new_words_bart > max_value_bart:
+            max_id_bart = line_bart['id']
+            max_value_bart = new_words_bart 
                 
         for word in long:
             if word not in base:
@@ -78,11 +91,17 @@ with jsonlines.open(base, 'r') as f_base, \
         if new_words_long > 0:
             counter_long = counter_long + 1
         arr_new_words_long.append(new_words_long)
+        if new_words_long > max_value_long:
+            max_id_long = line_long['id']
+            max_value_long = new_words_long 
     
     
     print("counter_bart", counter_bart/full_len)
     print("counter_pegasus", counter_pegasus/full_len)
     print("counter_long", counter_long/full_len)
     overview(arr_new_words_bart, "bart")
+    print("%d new in doc %s bart" % (max_value_bart ,max_id_bart))
     overview(arr_new_words_pegasus, "pegasus")
+    print("%d new in doc %s pegasus" % (max_value_pegasus ,max_id_pegasus))
     overview(arr_new_words_long, "long")
+    print("%d new in doc %s long" % (max_value_long ,max_id_long))
