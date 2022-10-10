e59984fa58301984848ab68c00f514cfd6ab2393
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-10-10 09:38:57 +0200

    adding counter for qrels that have an ndcg of zero after sum

--- bin/tools/analyse/run_analyse.py
+++ bin/tools/analyse/run_analyse.py
@@ -11,10 +11,12 @@ for f_name in [bart_path, long_path, pegasus_path]:
     worse_5 = []
     same_5 = []
     better_5 = []
+    now_zero_5 = []
     
     worse_10 = []
     same_10 = []
-    better_10 = []    
+    better_10 = []
+    now_zero_10 = []    
     print("Datei: %s" % (f_name,))
     with open(base_path, 'r') as f_base, \
             open(f_name, 'r') as f_current:
@@ -29,22 +31,30 @@ for f_name in [bart_path, long_path, pegasus_path]:
                 if ndcg_base == ndcg_current:
                     same_5.append(base_split[1])
                 if ndcg_base > ndcg_current:
-                    worse_5.append(base_split[1])
+                    if ndcg_current == 0:
+                        now_zero_5.append(base_split[1])
+                    else:
+                        worse_5.append(base_split[1])
                 if ndcg_base < ndcg_current:
                     better_5.append(base_split[1])
             else:
                 if ndcg_base == ndcg_current:
                     same_10.append(base_split[1])
                 if ndcg_base > ndcg_current:
-                    worse_10.append(base_split[1])
+                    if ndcg_current == 0:
+                        now_zero_10.append(base_split[1])
+                    else:
+                        worse_10.append(base_split[1])
                 if ndcg_base < ndcg_current:
                     better_10.append(base_split[1])
             counter = counter + 1
     print("better @5: %d" % (len(better_5)))
     print("same @5: %d" % (len(same_5)))
     print("worse @5: %d" % (len(worse_5)))
+    print("now zero @5: %d" % (len(now_zero_5)))
     
     print("better @10: %d" % (len(better_10)))
     print("same @10: %d" % (len(same_10)))
     print("worse @10: %d" % (len(worse_10)))
+    print("now zero @5: %d" % (len(now_zero_10)))
     
