3394f96d52333ff263bee9a0c073a93db5358e3d
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-10-20 09:02:43 +0200

    bugfix

--- bin/tools/qrel_tools/compare_qrel.py
+++ bin/tools/qrel_tools/compare_qrel.py
@@ -3,7 +3,7 @@ from read_10qrels import read_qrels
     
 if __name__ == '__main__':
     relevant_qrels = read_qrels()
-    print(out_arr[0])
-    print(out_arr[1])
-    print(out_arr[-1])
-    print(len(out_arr))
+    print(relevant_qrels[0])
+    print(relevant_qrels[1])
+    print(relevant_qrels[-1])
+    print(len(relevant_qrels))
