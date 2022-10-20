72beea4092b81c268a1a4d68d35bcf59bbe2ce14
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-10-20 09:44:54 +0200

    bugfix

--- bin/tools/qrel_tools/compare_qrel.py
+++ bin/tools/qrel_tools/compare_qrel.py
@@ -14,20 +14,20 @@ def get_qrel_from_id(id):
 def compare_base_bart(id):
     base_path = r'/home/jschmolzi/anserini/runs/run.base.txt'
     bart_path = '/home/jschmolzi/anserini/runs/run.bart.txt'
-    for line_base, line_bart in zip(base_path, bart_path):
-        print(line_base)
-        base_split = line_base.split()
-        base_id = base_split[0]
-        base_rank = int(base_split[3])
-        base_dokid = base_split[4]
-        
-        bart_split = line_bart.split()
-        bart_id = bart_split[0]
-        bart_rank = int(bart_split[3])
-        bart_dokid = bart_split[4]
-        
-        if base_id == id and base_rank < 11:
-            print("base %d: %s --- bart %d: %s" % (base_id, base_dokid, bart_id, bart_dokid))
+    with open(base_path, 'r') as base_f, open(bart_path, 'r') as bart_f:
+        for line_base, line_bart in zip(base_f, bart_f):
+            base_split = line_base.split()
+            base_id = base_split[0]
+            base_rank = int(base_split[3])
+            base_dokid = base_split[4]
+            
+            bart_split = line_bart.split()
+            bart_id = bart_split[0]
+            bart_rank = int(bart_split[3])
+            bart_dokid = bart_split[4]
+            
+            if base_id == id and base_rank < 11:
+                print("base %d: %s --- bart %d: %s" % (base_id, base_dokid, bart_id, bart_dokid))
         
         
 def get_qrel_info(id):
