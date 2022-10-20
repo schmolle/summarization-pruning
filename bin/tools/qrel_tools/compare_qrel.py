ba0641c33b00b1bccaad395a49f7f51bea172628
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-10-20 09:01:05 +0200

    adding tool to compare run files

---
+++ bin/tools/qrel_tools/compare_qrel.py
@@ -0,0 +1,9 @@
+from qrel_defs import qrel10_file_path
+from read_10qrels import read_qrels
+    
+if __name__ == '__main__':
+    relevant_qrels = read_qrels()
+    print(out_arr[0])
+    print(out_arr[1])
+    print(out_arr[-1])
+    print(len(out_arr))
