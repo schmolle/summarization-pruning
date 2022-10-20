826dc3b242f2bcb1ae81f6f14afa82254e8b2fa7
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-10-20 09:20:16 +0200

    reading qrel from qrel_id

--- bin/tools/qrel_tools/compare_qrel.py
+++ bin/tools/qrel_tools/compare_qrel.py
@@ -1,9 +1,17 @@
 from qrel_defs import qrel10_file_path
 from read_10qrels import read_qrels
     
+    
+def get_qrel_from_id(id):
+    path = '/home/jschmolzi/data/qrels/msmarco-docdev-queries.tsv'
+    with open(path, 'r') as f:
+        for line in f:
+            f_split = f.split()
+            f_id = int(f_split[0])
+            if id == f_id:
+                print("%d: %s" % (f_id, ' '.join(f_split[1,])))
+    
 if __name__ == '__main__':
     relevant_qrels = read_qrels()
-    print(relevant_qrels[0])
-    print(relevant_qrels[1])
-    print(relevant_qrels[-1])
-    print(len(relevant_qrels))
+    get_qrel_from_id(1101279)
+    
