ba1d2f5629c62076466d75d199d087b67cf831ca
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-10-20 09:24:13 +0200

    adding param

--- bin/tools/qrel_tools/compare_qrel.py
+++ bin/tools/qrel_tools/compare_qrel.py
@@ -1,6 +1,6 @@
 from qrel_defs import qrel10_file_path
 from read_10qrels import read_qrels
-    
+import sys
     
 def get_qrel_from_id(id):
     path = '/home/jschmolzi/data/qrels/msmarco-docdev-queries.tsv'
@@ -10,8 +10,12 @@ def get_qrel_from_id(id):
             f_id = int(f_split[0])
             if id == f_id:
                 print("%d: %s" % (f_id, ' '.join(f_split[1:])))
+
+def compare_base_bart():
+    base_path = r'/home/jschmolzi/anserini/runs/eval/base.txt'
+    bart_path = '/home/jschmolzi/anserini/runs/eval/bart.txt'
     
 if __name__ == '__main__':
     relevant_qrels = read_qrels()
-    get_qrel_from_id(1101279)
+    get_qrel_from_id(sys.argv[1])
     
