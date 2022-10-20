8b2611098eed87b025d7da9ea09c2ff241e4e869
Schmolle <Jean-Luc.Schmolzi@web.de>
2022-10-20 09:39:25 +0200

    bugfix

--- bin/tools/qrel_tools/compare_qrel.py
+++ bin/tools/qrel_tools/compare_qrel.py
@@ -11,12 +11,30 @@ def get_qrel_from_id(id):
             if id == f_id:
                 print("%s: %s" % (f_id, ' '.join(f_split[1:])))
 
-def compare_base_bart():
-    base_path = r'/home/jschmolzi/anserini/runs/eval/base.txt'
-    bart_path = '/home/jschmolzi/anserini/runs/eval/bart.txt'
-    
+def compare_base_bart(id):
+    base_path = r'~/anserini/runs/run.base.txt'
+    bart_path = '~/anserini/runs/run.bart.txt'
+    for line_base, line_bart in zip(base_path, bart_path):
+        base_split = line_base.split()
+        base_id = base_split[0]
+        base_rank = int(base_split[3])
+        base_dokid = base_split[4]
+        
+        bart_split = line_bart.split()
+        bart_id = bart_split[0]
+        bart_rank = int(bart_split[3])
+        bart_dokid = bart_split[4]
+        
+        if base_id == id and base_rank < 11:
+            print("base %d: %s --- bart %d: %s" % (base_id, base_dokid, bart_id, bart_dokid))
+        
+        
+def get_qrel_info(id):
+    get_qrel_from_id(id)
+    compare_base_bart(id)
+        
+        
 if __name__ == '__main__':
     relevant_qrels = read_qrels()
-    get_qrel_from_id('1000000')
-    get_qrel_from_id(sys.argv[1])
+    get_qrel_info(sys.argv[1])
     
